42ad5876b73d3bdb1399f04583fcafae3b27daf6
ipetrash <ilya.petrash@inbox.ru>
2017-09-13 14:43:55 +0500

    #

--- commands/test.py
+++ commands/test.py
@@ -4,14 +4,40 @@
 __author__ = 'ipetrash'
 
 
+# command = 'регнись петр иваныч регнись'
+# command_name = 'регнись'
+# word_list = command.split()
+#
+# for i in range(1, len(word_list) + 1):
+#     print(i, word_list[:i], ' '.join(word_list[:i]))
+#
+#     if command_name == ' '.join(word_list[:i]):
+#         print(command_name + ' ' + ' '.join(word_list[i:]))
+#         break
+#
+# quit()
+
 import commands
 result = commands.execute('команды')
-print(result)
+print(repr(result))
 print()
 
 result = commands.execute('ругнись петр иваныч')
-print(result)
+print(repr(result))
 print()
+
+result = commands.execute('регнись петр иваныч')
+print(repr(result))
+print()
+
+result = commands.execute('регнись регнись петр иваныч')
+print(repr(result))
+print()
+
+result = commands.execute('регнись петр иваныч регнись')
+print(repr(result))
+print()
+
 
 # result = commands.execute('спой')
 # print(result)
