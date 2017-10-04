89fc58c3f725e1c0ae6ac63462dc472b2a376c94
ipetrash <ilya.petrash@inbox.ru>
2017-10-04 17:41:40 +0500

    #

--- commands/command__weather_in_city/test.py
+++ commands/command__weather_in_city/test.py
@@ -11,6 +11,12 @@ import requests
 rs = requests.post('http://127.0.0.1:55003/execute', json=generate_request('Магнитогорск'))
 print(rs.json())
 
+rs = requests.post('http://127.0.0.1:55003/execute', json=generate_request('Челябинск'))
+print(rs.json())
+
+rs = requests.post('http://127.0.0.1:55003/execute', json=generate_request('Москва'))
+print(rs.json())
+
 rs = requests.post('http://127.0.0.1:55003/execute', json=generate_request('3421выаы:)'))
 print(rs.json())
 
