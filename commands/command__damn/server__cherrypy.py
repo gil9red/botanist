11a8e57e543a40913d013051a61d61b293aa0c5d
ipetrash <ilya.petrash@inbox.ru>
2017-10-04 17:28:48 +0500

    Добавлен сервер cherrypy команды "Ругнись"

---
+++ commands/command__damn/server__cherrypy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from commands.base_server import BaseServer
+from commands import DEBUG
+
+
+class DamnServer(BaseServer):
+    def __init__(self):
+        super().__init__()
+
+        self.name = 'DamnServer'
+
+    def _execute_body(self, rq=None):
+        if 'command' not in rq:
+            raise Exception("В запросе не найдено поле 'command'.")
+
+        command = rq['command']
+        if not command:
+            raise Exception("Неправильная команда 'ругнись': не указано на кого нужно ругнуться.")
+
+        if DEBUG:
+            result = command.upper()
+        else:
+            from commands.command__damn.damn import damn
+            result = damn(command)
+
+        ok = result is not None
+
+        rs = self.generate_response(result, ok)
+        if DEBUG:
+            print('  rs[DEBUG]:', rs)
+        else:
+            print('  rs:', rs)
+
+        return rs
+
+
+if __name__ == '__main__':
+    server = DamnServer()
+    server.run(port=55001)
