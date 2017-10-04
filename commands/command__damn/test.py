07f6ae2d9523193aae72a41be07b184a95d40240
ipetrash <ilya.petrash@inbox.ru>
2017-10-04 17:41:02 +0500

    #

--- commands/command__damn/test.py
+++ commands/command__damn/test.py
@@ -10,6 +10,12 @@ import requests
 rs = requests.post('http://127.0.0.1:55001/execute', json=generate_request('Петя'))
 print(rs.json())
 
+rs = requests.post('http://127.0.0.1:55001/execute', json=generate_request())
+print(rs.json())
+
+rs = requests.post('http://127.0.0.1:55001/execute', json=generate_request(''))
+print(rs.json())
+
 rs = requests.post('http://127.0.0.1:55001/execute', json={'status': 'ok'})
 print(rs.json())
 
