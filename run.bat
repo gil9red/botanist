4d1e768b5b5a7c961b29c061cbcfaa33f2afde60
ipetrash <ilya.petrash@inbox.ru>
2017-10-05 11:50:41 +0500

    Добавление сервера HexConvert

--- run.bat
+++ run.bat
@@ -2,11 +2,12 @@
 REM Указание пути, чтобы модули при импортировании могли найти нужные им модули/пакеты
 REM . -- текущая папка
 set PYTHONPATH=.
-set PYTHON=C:\ProgramData\Anaconda3\python.exe
+set PYTHON=C:\Users\ipetrash\AppData\Local\Continuum\Anaconda3\python.exe
 
 start %PYTHON% main.py
 start %PYTHON% commands\coordinator.py
 start %PYTHON% commands\command__damn\server.py
 start %PYTHON% commands\command__exchange_rate\server.py
 start %PYTHON% commands\command__fun\server.py
+start %PYTHON% commands\command__hex_converter\server.py
 start %PYTHON% commands\command__weather_in_city\server.py
