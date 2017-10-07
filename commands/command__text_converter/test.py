bf67161bd6d1c9ea09b2ca52cdbcc2106598db9b
ipetrash <ilya.petrash@inbox.ru>
2017-10-08 00:09:54 +0500

    #

--- commands/command__text_converter/test.py
+++ commands/command__text_converter/test.py
@@ -12,20 +12,33 @@ url = get_url_server(TextConverter.guid)
 
 
 import requests
+
 rs = requests.post(
     url + '/execute?str2hex',
     json=generate_request(command_name='str2hex', command='Привет мир!')
 )
 print(rs.json())
 
-import requests
 rs = requests.post(
     url + '/execute?hex2str',
     json=generate_request(command_name='hex2str', command='CFF0E8E2E5F220ECE8F021')
 )
 print(rs.json())
 
-import requests
+rs = requests.post(
+    url + '/execute?str2bin',
+    json=generate_request(command_name='str2bin', command='Привет мир!')
+)
+print(rs.json())
+
+rs = requests.post(
+    url + '/execute?bin2str',
+    json=generate_request(command_name='bin2str', command='11001111 11110000 11101000 11100010 11100101 11110010 '
+                                                          '00100000 11101100 11101000 11110000 00100001')
+)
+print(rs.json())
+
+# Специально вызываем ошибку
 rs = requests.post(
     url + '/execute?hex2str',
     json=generate_request(command_name='hex2str', command='Привет мир!')
