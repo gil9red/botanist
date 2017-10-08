7eb390cf07a1329f41f657dc13137e2f9e515d27
ipetrash <ilya.petrash@inbox.ru>
2017-10-09 00:43:15 +0500

    Небольшой рефакторинг

--- main.py
+++ main.py
@@ -27,15 +27,11 @@ import sys
 sys.excepthook = log_uncaught_exceptions
 
 
+import commands
 import time
 
 
-if __name__ == '__main__':
-    import vk_api
-    vk = vk_api.VkApi(login=LOGIN, password=PASSWORD)
-    vk.auth()
-    log.debug("Бот запущен")
-
+def messages_get(vk):
     command_prefix = 'Бот,'
 
     messages_get_values = {
@@ -44,72 +40,74 @@ if __name__ == '__main__':
         'version': '5.68'
     }
 
-    def messages_get():
-        rs = vk.method('messages.get', messages_get_values)
-        # log.debug(rs)
+    rs = vk.method('messages.get', messages_get_values)
+    # log.debug(rs)
 
-        # Если ничего не пришло
-        if not rs['items']:
-            return
+    # Если ничего не пришло
+    if not rs['items']:
+        return
 
-        item = rs['items'][0]
-        message_id = item['id']
-        from_user_id = item['user_id']
-        message = item['body']
+    item = rs['items'][0]
+    message_id = item['id']
+    from_user_id = item['user_id']
+    message = item['body']
 
-        # Если сообщение пришло из групповой беседы, в chat_id будет число, иначе None
-        chat_id = item.get('chat_id')
+    # Если сообщение пришло из групповой беседы, в chat_id будет число, иначе None
+    chat_id = item.get('chat_id')
 
-        # Бот реагирует только на сообщения, начинающиеся с префикса
-        if not message.lower().startswith(command_prefix.lower()):
-            return
+    # Бот реагирует только на сообщения, начинающиеся с префикса
+    if not message.lower().startswith(command_prefix.lower()):
+        return
 
-        if chat_id:
-            log.debug('From chat #%s from user #%s, message (#%s): "%s"',
-                      chat_id, from_user_id, message_id, message)
-        else:
-            log.debug('From user #%s, message (#%s): "%s"', from_user_id, message_id, message)
+    if chat_id:
+        log.debug('From chat #%s from user #%s, message (#%s): "%s"',
+                  chat_id, from_user_id, message_id, message)
+    else:
+        log.debug('From user #%s, message (#%s): "%s"', from_user_id, message_id, message)
 
-        command = message[len(command_prefix):].strip()
+    command = message[len(command_prefix):].strip()
 
-        # Выполнение команды
-        try:
-            import commands
-            message = commands.execute(command)
+    # Выполнение команды
+    try:
+        message = commands.execute(command)
 
-        except Exception as e:
-            log.exception("Error:")
+    except Exception as e:
+        log.exception("Error:")
 
-            import traceback
-            message = 'При выполнении команды "{}" произошла ошибка: ' \
-                      '"{}":\n\n{}'.format(command, e, traceback.format_exc())
+        import traceback
+        message = 'При выполнении команды "{}" произошла ошибка: ' \
+                  '"{}":\n\n{}'.format(command, e, traceback.format_exc())
 
-        # Если ответа от бота нет
-        if not message:
-            message = 'Не получилось выполнить команду "{}" :( Попробуй позже повторить :)'.format(command)
+    # Если ответа от бота нет
+    if not message:
+        message = 'Не получилось выполнить команду "{}" :( Попробуй позже повторить :)'.format(command)
 
-        log.debug('Message: "%s"', message)
+    log.debug('Message: "%s"', message)
 
-        messages_send_values = {
-            'message': message,
-            'version': '5.67'
-        }
+    messages_send_values = {
+        'message': message,
+        'version': '5.67'
+    }
 
-        # Если сообщение пришло из групповой беседы
-        if chat_id:
-            messages_send_values['chat_id'] = chat_id
-        else:
-            messages_send_values['user_id'] = from_user_id
+    # Если сообщение пришло из групповой беседы
+    if chat_id:
+        messages_send_values['chat_id'] = chat_id
+    else:
+        messages_send_values['user_id'] = from_user_id
 
-        time.sleep(0.3)
+    last_message_bot_id = vk.method('messages.send', messages_send_values)
+    messages_get_values['last_message_id'] = last_message_bot_id
 
-        last_message_bot_id = vk.method('messages.send', messages_send_values)
-        messages_get_values['last_message_id'] = last_message_bot_id
 
+if __name__ == '__main__':
+    import vk_api
+    vk = vk_api.VkApi(login=LOGIN, password=PASSWORD)
+    vk.auth()
+    log.debug("Бот запущен")
 
     while True:
         try:
-            messages_get()
+            messages_get(vk)
 
         except Exception as e:
             log.exception('Error:')
