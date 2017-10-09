83b9b52ae80f9dafee7ed7e51558525f688e233e
ipetrash <ilya.petrash@inbox.ru>
2017-10-09 21:30:04 +0500

    #

--- run.bat
+++ run.bat
@@ -2,12 +2,13 @@
 REM Указание пути, чтобы модули при импортировании могли найти нужные им модули/пакеты
 REM . -- текущая папка
 set PYTHONPATH=.
-set PYTHON=C:\Users\ipetrash\AppData\Local\Continuum\Anaconda3\python.exe
+set PYTHON=C:\ProgramData\Anaconda3\python.exe
 
 start %PYTHON% main.py
 start %PYTHON% commands\coordinator.py
 start %PYTHON% commands\command__damn\server.py
 start %PYTHON% commands\command__exchange_rate\server.py
 start %PYTHON% commands\command__fun\server.py
+start %PYTHON% commands\command__test_get_attachment\server.py
 start %PYTHON% commands\command__text_converter\server.py
 start %PYTHON% commands\command__weather_in_city\server.py
