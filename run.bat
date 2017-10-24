ed2cb0cb668cfc49891551921871fa9de024894c
ipetrash <ilya.petrash@inbox.ru>
2017-10-25 02:28:26 +0500

    #

--- run.bat
+++ run.bat
@@ -4,7 +4,9 @@ set PYTHON=C:\ProgramData\Anaconda3\python.exe
 
 start %PYTHON% main.py
 start %PYTHON% commands\coordinator.py
+start %PYTHON% commands\command__calc\server.py
 start %PYTHON% commands\command__damn\server.py
+start %PYTHON% commands\command__duckduckgo\server.py
 start %PYTHON% commands\command__exchange_rate\server.py
 start %PYTHON% commands\command__fun\server.py
 start %PYTHON% commands\command__test_get_attachment\server.py
