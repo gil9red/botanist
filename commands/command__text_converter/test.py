d755dbe800dfbafe7f65bdc290eac5924c146b0b
ipetrash <ilya.petrash@inbox.ru>
2017-10-27 15:24:47 +0500

    Проверка полученных значений в тесте

--- commands/command__text_converter/test.py
+++ commands/command__text_converter/test.py
@@ -18,18 +18,22 @@ rs = requests.post(
     json=generate_request(command_name='str2hex', command='Привет мир!')
 )
 print(rs.json())
+assert rs.json()['result'] == 'CFF0E8E2E5F220ECE8F021'
 
 rs = requests.post(
     url + '/execute?hex2str',
     json=generate_request(command_name='hex2str', command='CFF0E8E2E5F220ECE8F021')
 )
 print(rs.json())
+assert rs.json()['result'] == 'Привет мир!'
 
 rs = requests.post(
     url + '/execute?str2bin',
     json=generate_request(command_name='str2bin', command='Привет мир!')
 )
 print(rs.json())
+assert rs.json()['result'] == '11001111 11110000 11101000 11100010 11100101 11110010 ' \
+                              '00100000 11101100 11101000 11110000 00100001'
 
 rs = requests.post(
     url + '/execute?bin2str',
@@ -37,6 +41,7 @@ rs = requests.post(
                                                           '00100000 11101100 11101000 11110000 00100001')
 )
 print(rs.json())
+assert rs.json()['result'] == 'Привет мир!'
 
 # Специально вызываем ошибку
 rs = requests.post(
@@ -44,3 +49,4 @@ rs = requests.post(
     json=generate_request(command_name='hex2str', command='Привет мир!')
 )
 print(rs.json())
+assert rs.json()['result'] is None
